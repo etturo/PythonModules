5ad7816a1d8ebac16b1e60c3ebed127ed9fd04e3
etturo <198966657+etturo@users.noreply.github.com>
2026-01-27 23:09:37 +0100

    finished ex2 od module 02

--- module02/ex2/ft_custom_errors.py
+++ module02/ex2/ft_custom_errors.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python3
 
 class GardenError(Exception):
-    def __init__(self, message=None):
+    def __init__(self, message=None) -> None:
         self.error_message = message
         super().__init__(self, message)
+        return
 
-    def __str__(self):
+
+class PlantError(GardenError):
+    def __init__(self, message=None) -> None:
+        self.error_message = message
+        super().__init__(message)
+        return
+
+    def __str__(self) -> str:
         return ("The tomato plant is wilting!")
 
 
 class WaterError(GardenError):
-    def __init__(self, message=None):
+    def __init__(self, message=None) -> None:
         self.error_message = message
         super().__init__(message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ("Not enough water in the tank!")
 
 
 if __name__ == "__main__":
     print("\n=== Custom Garden Errors Demo ===\n")
     # Trying to catch a PlantError
+    print("Testing a PlantError...")
     try:
-        print("Testing a PlantError...")
-        raise GardenError("Caught PlantError: ")
-    except GardenError as error:
-        print(f"Error: {error}")
+        raise PlantError("Caught PlantError")
+    except PlantError as error:
+        print(f"{error.error_message}: {error}")
     # Trying to catch a WaterError
+    print("\nTesting a WaterError...")
     try:
-        print("\nTesting a WaterError...")
-        raise WaterError("Caught WaterPlant: ")
+        raise WaterError("Caught WaterError")
     except WaterError as error:
-        print(f"Error: {error}")
-    
+        print(f"{error.error_message}: {error}")
+    # Trying to catch all garden errors
+    print("\nTesting catching all garden errors...")
+    try:
+        raise PlantError("Caught PlantError")
+    except GardenError as error:
+        print(f"{error.error_message}: {error}")
+    try:
+        raise WaterError("Caught WaterError")
+    except GardenError as error:
+        print(f"{error.error_message}: {error}")
     print("\nAll custom error types work correctly!\n")
