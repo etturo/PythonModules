2ace257d3d00ee02cea35fbe48e94c8dc46d8f9a
Ettore <198966657+etturo@users.noreply.github.com>
2026-01-28 16:39:28 +0100

    addded ex 1 in mod3

---
+++ module03/ex1/ft_score_analytics.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+    scores = []
+    if len(sys.argv) == 1:
+        return print(f"No scores provided. "
+                     f"Usage: python3 {sys.argv[0]} <score1> <score2> ...")
+    try:
+        i = 1
+        while i < len(sys.argv):
+            scores.append(int(sys.argv[i]))
+            i += 1
+    except ValueError:
+        return print("Error: non numerical value inserted")
+    print(f"Scores processed: {scores}")
+    print(f"Total player: {len(sys.argv) - 1}")
+    print(f"Total Score: {sum(scores)}")
+    print(f"Avarage score: {sum(scores) / (len(sys.argv) - 1)}")
+    print(f"High score: {max(scores)}")
+    print(f"Low score: {min(scores)}")
+    print(f"Score range: {max(scores) - min(scores)}")
+
+
+if __name__ == "__main__":
+    print("\n=== Player Score Analytics ===\n")
+    main()
+    print()
