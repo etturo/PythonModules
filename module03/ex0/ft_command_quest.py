56cc847193eb3d2eb1791900d619d660c2330c61
Ettore <198966657+etturo@users.noreply.github.com>
2026-01-28 15:41:33 +0100

    added module 03 ex0

---
+++ module03/ex0/ft_command_quest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main() -> int:
+    print("=== Command Quest ===")
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) == 1:
+        return print(f"Total arguments: {len(sys.argv)}")
+    print(f"Arguments received: {len(sys.argv) - 1}")
+    i = 1
+    while i < len(sys.argv):
+        print(f"Arguments {i}: {sys.argv[i]}")
+        i += 1
+    print(f"Total arguments: {len(sys.argv)}")
+    return
+
+
+if __name__ == "__main__":
+    main()
