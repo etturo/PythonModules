97db78911269d886b66c62c4f440305d532abf5a
Ettore <198966657+etturo@users.noreply.github.com>
2026-02-05 13:31:53 +0100

    pausa pranzo

--- module05/ex1/data_stream.py
+++ module05/ex1/data_stream.py
@@ -2,4 +2,74 @@
 from typing import Any, List, Dict, Union, Optional
 from abc import ABC, abstractmethod
 
+
+class DataStream(ABC):
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
+        self.stats : Dict[str, Union[str, float, int]] = {}
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str]
+= None) -> List[Any]:
+        return data_batch
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+class SensorStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str]
+= None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id):
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            total: int = sum(data_batch)
+            avarage: int = total / len(data_batch)
+            return avarage
+        except TypeError:
+            print("Error: Non numeric value inserted!")
+
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str]
+= None) -> List[Any]:
+        if criteria == "high_pass":
+            return [x for x in data_batch if isinstance(x, Union(int, float))
+                    and x > 20]
+        return data_batch
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+
+
+class EventStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str]
+= None) -> List[Any]:
+        
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+
+
+class StreamProcessor():
+    def __init__(self):
+        pass
+
+def main():
+    print()
+
+
 if __name__ == "__main__":
+    print("\n=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
+    main()
+    print()
