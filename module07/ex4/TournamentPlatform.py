e48bb4f6c16b9b168c1035667310f34eb97c4b2e
Ettore <198966657+etturo@users.noreply.github.com>
2026-02-23 16:33:32 +0100

    finished M07

--- module07/ex4/TournamentPlatform.py
+++ module07/ex4/TournamentPlatform.py
@@ -4,6 +4,8 @@ from ex4.TournamentCard import TournamentCard
 
 class TournamentPlatform():
     _card_registered: list[TournamentCard] = []
+    _number_of_matches: int = 0
+    _status: str = "Inactive"
 
     def register_card(self,
                       card: TournamentCard) -> str:
@@ -12,10 +14,50 @@ class TournamentPlatform():
     def create_match(self,
                      card1_id: str,
                      card2_id: str) -> dict:
-        pass
+        self._status = "Active"
+
+        card1: TournamentCard = next(
+            card for card in self._card_registered
+            if card._info['name'] == card1_id)
+
+        card2: TournamentCard = next(
+            card for card in self._card_registered
+            if card._info['name'] == card2_id)
+
+        if card1._info['cost'] >= card2._info['cost']:
+            winner: TournamentCard = card1
+            loser: TournamentCard = card2
+        else:
+            winner: TournamentCard = card2
+            loser: TournamentCard = card1
+
+        winner.update_wins(1)
+        winner._rating += 8
+        loser.update_losses(1)
+        loser._rating -= 8
+
+        self._number_of_matches += 1
+
+        return {"winner": winner,
+                "loser": loser,
+                "winner_rating": winner._rating,
+                "loser_rating": loser._rating}
 
     def get_leaderboard(self) -> list:
-        pass
+        return sorted(self._card_registered,
+                      key=lambda card: card._rating,
+                      reverse=True)
 
     def generate_tournament_report(self) -> dict:
-        pass
+        total_rating: int = 0
+        avarage_rating: float
+
+        for card in self._card_registered:
+            total_rating += card._rating
+
+        avarage_rating = total_rating / len(self._card_registered)
+
+        return {'total_card': len(self._card_registered),
+                'matches_played': self._number_of_matches,
+                'avg_rating': avarage_rating,
+                'platform_status': self._status}
--- module07/ex4/main.py
+++ module07/ex4/main.py
@@ -1,6 +1,5 @@
 #!/usd/bin/env python3
 from ex4.TournamentCard import TournamentCard
-from ex4.Rankable import Rankable
 from ex4.TournamentPlatform import TournamentPlatform
 
 
@@ -25,3 +24,26 @@ if __name__ == "__main__":
     print()
 
     print("Creating tournament match")
+    print("Mach result: "
+          f"{manager.create_match('Fire Dragon', 'Ice Wizard')}")
+
+    print()
+
+    print("Tournament Leadboard:")
+    leadboard: list[TournamentCard] = manager.get_leaderboard()
+    it: int = 1
+    for card in leadboard:
+        print(f"{it} - {leadboard[it - 1]._info['name']}"
+              f" - Rating: {leadboard[it - 1]._rating} "
+              f"({leadboard[it - 1]._wins}-{leadboard[it - 1]._loss})")
+        it += 1
+
+    print()
+
+    print("Platform Report:")
+    print(manager.generate_tournament_report())
+
+    print()
+
+    print("=== Tournament Platform Successfully Deployed! ===")
+    print("All abstract patterns working together harmoniously!")
